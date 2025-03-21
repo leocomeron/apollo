71336f51858d8e9ee4602d010ee7ad777a2588d8
Leandro Comeron <leocomeron@gmail.com>
2025-03-21 13:56:16 -0300

    fix: update session check to use status for redirection in profile page

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -23,10 +23,10 @@ const Profile = () => {
   const router = useRouter();
 
   useEffect(() => {
-    if (!session) {
+    if (status === 'unauthenticated') {
       void router.push('/login');
     }
-  }, [session, router]);
+  }, [status, router]);
 
   const user = session?.user;
 
